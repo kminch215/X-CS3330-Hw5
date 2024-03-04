4b00844aa9d500456f0f0c651b6d7b026e1f79e6
sarahkgreenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-03-03 23:34:41 -0600

    Update PizzaType.javac
    Create pizzatype enum

--- src/edu/mu/pizza/PizzaType.java
+++ src/edu/mu/pizza/PizzaType.java
@@ -1,5 +1,22 @@
-package edu.mu.pizza;
+package edu.mu.fakespeare;
 
 public enum PizzaType {
+	HAWAIIAN(Toppings.CANADIAN_BACON, Toppings.CHEESE, Toppings.PINEAPPLE),
+	MARGHERITA(Toppings.TOMATO, Toppings.CHEESE),
+	SUPREME(Toppings.TOMATO, Toppings.CHEESE, Toppings.BELL_PEPPER, Toppings.ITALIAN_SAUSAGE, Toppings.PEPPERONI, Toppings.BLACK_OLIVE, Toppings.MUSHROOM),
+	VEGETARIAN(Toppings.TOMATO, Toppings.CHEESE, Toppings.BELL_PEPPER, Toppings.BLACK_OLIVE, Toppings.MUSHROOM);
 
+	private Toppings[] toppings;
+
+    PizzaType(Toppings... toppings) {
+        this.toppings = toppings;
+    }
+
+    public double calculatePrice() {
+        double total = 0;
+        for (Toppings topping : toppings) {
+            total += topping.getToppingPrice();
+        }
+        return total;
+    }
 }
