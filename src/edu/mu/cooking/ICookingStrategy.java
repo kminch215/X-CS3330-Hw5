a874f610105b14fb629bbd04dbf7a7e4ce1e84a1
sarahkgreenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-03-06 14:18:25 -0600

    Update ICookingStrategy.java
    Upload ICookingStrategy

--- src/edu/mu/cooking/ICookingStrategy.java
+++ src/edu/mu/cooking/ICookingStrategy.java
@@ -1,5 +1,49 @@
 package edu.mu.cooking;
 
+import edu.mu.pizza.AbstractPizza;
+import edu.mu.pizza.HawaiianPizza;
+import edu.mu.pizza.MargheritaPizza;
+import edu.mu.pizza.PizzaType;
+import edu.mu.pizza.SupremePizza;
+import edu.mu.pizza.VegetarianPizza;
+
 public interface ICookingStrategy {
 
+public default boolean cook(AbstractPizza pizza) {
+	return true;	
+}
+
+class BrickOvenCookingStrategy implements ICookingStrategy {
+
+	@Override
+	public boolean cook(AbstractPizza pizza) {
+		int cookingPrice = 10;
+		String CookingStrat = "Brick Oven";
+		//AbstractPizza.pizzaprice += cookingPrice;
+		return false;
+	}
+		
+}
+class ConventionalOvenCookingStrategy implements ICookingStrategy {
+
+	@Override
+	public boolean cook(AbstractPizza pizza) {
+		int cookingPrice = 8;
+		String CookingStrat = "Conventional Oven";
+		//AbstractPizza.pizzaprice += cookingPrice;		
+		return false;
+	}
+		
+}
+class MicrowaveCookingStrategy implements ICookingStrategy {
+
+	@Override
+	public boolean cook(AbstractPizza pizza) {
+		int cookingPrice = 1;
+		String CookingStrat = "Microwave";
+		//AbstractPizza.pizzaprice += cookingPrice;
+		return false;
+	}
+		
+}
 }
