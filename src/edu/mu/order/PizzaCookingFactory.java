f1cabfcfff580bba24ac81fb4934400be478017d
kminch215 <kendraminch1036@gmail.com>
2024-03-06 09:30:31 -0600

    Creating the main PizzaCookingFactory method that creates the pizzas
    This will create a pizza according to the type that is passed to
    createPizza.

--- src/edu/mu/order/PizzaCookingFactory.java
+++ src/edu/mu/order/PizzaCookingFactory.java
@@ -1,5 +1,36 @@
 package edu.mu.order;
 
-public class PizzaCookingFactory {
+import edu.mu.pizza.AbstractPizza;
+import edu.mu.pizza.HawaiianPizza;
+import edu.mu.pizza.MargheritaPizza;
+import edu.mu.pizza.PizzaType;
+import edu.mu.pizza.SupremePizza;
+import edu.mu.pizza.VegetarianPizza;
 
+public class PizzaCookingFactory {
+	
+	//constructor
+	public PizzaCookingFactory() {
+	}
+
+	//createPizza method that will create a pizza according to the PizzaType that is passed
+	//each of the pizza constructors will update the orderIDCount and the pizzaOrderID for each pizza created
+	public AbstractPizza createPizza(PizzaType pizzaType) {
+		switch(pizzaType) {
+			case HAWAIIAN:
+				return new HawaiianPizza();
+			case MARGHERITA:
+				return new MargheritaPizza();
+			case SUPREME:
+				return new SupremePizza();
+			case VEGETARIAN:
+				return new VegetarianPizza();
+			default:
+				System.out.println("Ivalid Pizza");
+				break;
+		}
+		
+		return null;
+	}
+	
 }
