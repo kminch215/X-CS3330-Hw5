25c179638ec3ab5564c1c6f97d844ce955821698
sarahkgreenwood <149001674+sarahkgreenwood@users.noreply.github.com>
2024-03-11 11:07:12 -0500

    Update ICookingStrategy.java
    Finalized

--- src/edu/mu/cooking/ICookingStrategy.java
+++ src/edu/mu/cooking/ICookingStrategy.java
@@ -1,26 +1,25 @@
-package edu.mu.cooking;
+package edu.mu.fakespeare;
 
-import edu.mu.pizza.AbstractPizza;
-import edu.mu.pizza.HawaiianPizza;
-import edu.mu.pizza.MargheritaPizza;
-import edu.mu.pizza.PizzaType;
-import edu.mu.pizza.SupremePizza;
-import edu.mu.pizza.VegetarianPizza;
+import edu.mu.fakespeare.AbstractPizza;
+import edu.mu.fakespeare.HawaiianPizza;
+import edu.mu.fakespeare.MargheritaPizza;
+import edu.mu.fakespeare.PizzaType;
+import edu.mu.fakespeare.SupremePizza;
+import edu.mu.fakespeare.VegetarianPizza;
 
 public interface ICookingStrategy {
 
 public default boolean cook(AbstractPizza pizza) {
-	return true;	
+	return true;
 }
 
 class BrickOvenCookingStrategy implements ICookingStrategy {
 
 	@Override
 	public boolean cook(AbstractPizza pizza) {
-		int cookingPrice = 10;
-		String CookingStrat = "Brick Oven";
-		//AbstractPizza.pizzaprice += cookingPrice;
-		return false;
+		pizza.cookingPrice = 10;
+		pizza.totalPrice += pizza.cookingPrice;
+		return true;
 	}
 		
 }
@@ -28,10 +27,9 @@ class ConventionalOvenCookingStrategy implements ICookingStrategy {
 
 	@Override
 	public boolean cook(AbstractPizza pizza) {
-		int cookingPrice = 8;
-		String CookingStrat = "Conventional Oven";
-		//AbstractPizza.pizzaprice += cookingPrice;		
-		return false;
+		pizza.cookingPrice = 8;
+		pizza.totalPrice += pizza.cookingPrice;
+		return true;
 	}
 		
 }
@@ -39,11 +37,19 @@ class MicrowaveCookingStrategy implements ICookingStrategy {
 
 	@Override
 	public boolean cook(AbstractPizza pizza) {
-		int cookingPrice = 1;
-		String CookingStrat = "Microwave";
-		//AbstractPizza.pizzaprice += cookingPrice;
-		return false;
+		pizza.cookingPrice = 1;
+		pizza.totalPrice += pizza.cookingPrice;
+		return true;
 	}
 		
 }
+/* juzt in case the return doesn't work in abstract
+ * string cookingStrat = "";
+ * if (pizza.cookingPrice ==1 ) {
+	cookingStrat = "Microwave"; }
+	if (pizza.cookingPrice == 8) {
+	cookingStrat = "Conventional Oven" }
+	if (pizza.cookingPrice == 10) {
+	cookingStrat = "Brick Oven) }
+*/	
 }
